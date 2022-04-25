f327a6200dcb18c005cb43b458fc0c7cfba25fbf
Jon Kaplon <jon.kaplon1@gmail.com>
2022-04-25 12:57:46 -0700

    commit tree 32aca7f5304b4b6d9d905d71f4b6c88f17795d36

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,42 @@
 # your code here
+
+
+class Stack
+    
+    def initialize(limit = nil)
+        @stack = []
+        @limit = limit
+    end
+
+    def push(val)
+        raise 'Stack Overflow' if full?
+        @stack.push(val)
+    end
+
+    def full?
+        @limit && @stack.size == @limit
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack.last
+    end
+
+    def size
+        @stack.size
+    end
+
+    def empty?
+        @stack.empty?
+    end
+
+    def search(val)
+        @stack.each_with_index do |item, idx|
+            return size - idx - 1 if item == val
+        end
+        -1
+    end
+end
